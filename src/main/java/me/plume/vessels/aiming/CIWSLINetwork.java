6e70e14de17ab53cf8c01e81265f2fc62fd95fea
SparkingToaster <plume.expansion@gmail.com>
2024-04-24 00:35:47 -0400

    CIWSLITrack/Network update
    null intercept angle protection
    added forceAim toggle for skipping over out of range targets

--- src/main/java/me/plume/vessels/aiming/CIWSLINetwork.java
+++ src/main/java/me/plume/vessels/aiming/CIWSLINetwork.java
@@ -31,18 +31,18 @@ public class CIWSLINetwork {
 		tracks.values().forEach(track -> {
 			track.availables.clear();
 			turrets.forEach(t -> {
-				if (!t.auto) return;
-				double theta = track.calcAngle(t);
+				if (!t.auto || !t.networked) return;
+				Double theta = track.calcAngle(t);
+				if (theta == null) return;
 				double angleFromBounds = t.angleFromBounds(theta);
 				if (angleFromBounds>0) track.availables.put(t, Navigator.angleDiff(theta, t.getAngle()));
 				else track.backups.put(t, angleFromBounds);
 			});
 		});
 		occupied.clear();
-		//TODO less dangerous missiles with deligN=2 seem to be hogging turrets
 		// assining available turrets
 		tracks.values().stream().filter(track -> track.deligN!=0).sorted((a, b) -> a.incptScore<b.incptScore? -1 : 1).forEach(track -> {
-			assign(track, track.availables, track.deligN, time, dt);
+			assign(track, track.availables, track.deligN, time, dt, false);
 		});
 		// aiming free turrets
 		if (occupied.size() < turrets.size()) {
@@ -54,17 +54,17 @@ public class CIWSLINetwork {
 				if (aShooting && bShooting) return a.lastShot-a.firstShot>b.lastShot-b.firstShot? -1 : 1;
 				return a.dta-(time-a.lastShot) < b.dta-(time-b.lastShot)? -1 : 1;
 			}).forEach(track -> {
-				assign(track, track.availables, 1, time, dt);
+				assign(track, track.availables, 1, time, dt, true);
 			});
 		}
 		// aiming unavailable turrets
 		if (occupied.size() < turrets.size()) {
-			turrets.stream().filter(t -> t.auto).forEach(t -> {
+			turrets.stream().filter(t -> t.auto && t.networked).forEach(t -> {
 				if (!occupied.contains(t)) t.shoot(false);
 			});
 		}
 	}
-	private void assign(CIWSLITrack track, Map<CIWSTurret, Double> availables, int n, double time, double dt) {
+	private void assign(CIWSLITrack track, Map<CIWSTurret, Double> availables, int n, double time, double dt, boolean forceAim) {
 		List<Entry<CIWSTurret, Double>> remaining = availables.entrySet().stream()
 				.filter(e -> !occupied.contains(e.getKey()))
 				.sorted((a, b) -> a.getValue()<b.getValue()? -1 : 1)
@@ -72,9 +72,10 @@ public class CIWSLINetwork {
 		for(int i = 0; i < Math.min(remaining.size(), n); i++) {
 			Entry<CIWSTurret, Double> e = remaining.get(i);
 			CIWSTurret t = e.getKey();
+			if (track.dta > t.life) continue;
 			occupied.add(t);
 			t.angle(track.calcAngle(t), dt);
-			if (e.getValue() <= CIWSLITrack.RAD_ON_TARGET && track.dta <= t.life) {
+			if (e.getValue() <= CIWSLITrack.RAD_ON_TARGET) {
 				if (track.firstShot == 0 || track.lastShot-track.firstShot>CIWSLITrack.SHOOT_TIME) track.firstShot = time;
 				t.shoot(true);
 				track.lastShot = time;
